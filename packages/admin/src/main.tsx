a7cc314471c8bbfadef3478203f0097cc8df56b1
laagom <cjyooong@gmail.com>
2025-11-03 19:29:10 +0900

    fork update

--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import RouteSetup from "./router/RouteSetup";
 
+// github fork 수정용
 createRoot(document.getElementById("root")!).render(<RouteSetup />);
